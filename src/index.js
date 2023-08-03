eb5be18412c74049cdc3609e589423a7051ee2e4
morganbarrett <morganbarrett@icloud.com>
2023-08-04 00:58:55 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import {View, ViewProps} from "react-native";
+
+export const Box = (props: ViewProps) => {
+	const {style, children} = props;
+
+	return (
+		<View
+			{...props}
+			style={[
+				{
+					margin: 16,
+					backgroundColor: "gray", //todo use color module (backgroundSecondary)
+					borderRadius: 10
+				},
+				style
+			]}
+		>
+			{children}
+		</View>
+	);
+};
