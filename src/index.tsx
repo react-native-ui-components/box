aae9a2dc11b313fb632126f77aac9aca737fada9
morganbarrett <morganbarrett@icloud.com>
2024-01-23 10:29:33 +0000

    feat: added HBox

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
 import {View, type ViewProps} from "react-native";
 
+export const HBox = ({style, children, ...props}: ViewProps) => (
+	<View {...props} style={[style, {flexDirection: "row"}]}>
+		{children}
+	</View>
+);
+
 export const Box = (props: ViewProps) => {
 	const {style, children} = props;
 
